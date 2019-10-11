8f0a29fb738485354a3b47639a3c7834bed151ff
Kevin Hoercher <khoerch@gmail.com>
2019-10-10 20:05:48 -0400

    Add forms

--- src/FolderForm/FolderForm.js
+++ src/FolderForm/FolderForm.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './FolderForm.css';
 
 class FolderForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            folder: {
+                value: ''
+            }
+        }
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault();
+        const folderTitle = this.state.folder.value
+        console.log(folderTitle)
+    }
+
+    updateFolder(folder) {
+        this.setState({ folder: {value: folder} })
+    }
+
 	render() {
 		return (
-                <form className='FolderForm'>
+                <form className='FolderForm' onSubmit={e => this.handleSubmit(e)}>
                     <legend>Add new folder:</legend>
-                    <label for="folder-title">Title</label>
-                    <input id="folder-title" type="text" placeholder="Very important folder"/>
+                    <label for="newFolder">Title</label>
+                    <input id="newFolder" type="text" placeholder="Very important folder" onChange={e => this.updateFolder(e.target.value)}/>
                     <input type="submit"/>
                 </form>
 		);
--- src/NotesForm/NotesForm.js
+++ src/NotesForm/NotesForm.js
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 import './NotesForm.css';
 
 class NotesForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: {
+                value: ''
+            },
+            body: {
+                value: ''
+            }
+        }
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault();
+        const { title, body } = this.state
+        console.log(title.value)
+        console.log(body.value)
+    }
+
+    updateTitle(title) {
+        this.setState({ title: {value: title} })
+    }
+
+    updateBody(body) {
+        this.setState({ body: {value: body} })
+    }
+
 	render() {
 		return (
-                <form className='NotesForm'>
+                <form className='NotesForm' onSubmit={e => this.handleSubmit(e)}>
                     <legend>Add new note:</legend>
                     <label htmlFor="note-title">Title</label>
-                    <input id="note-title" type="text" placeholder="Very important note"/>
+                    <input id="note-title" type="text" placeholder="Very important note" onChange={e => this.updateTitle(e.target.value)} />
                     <label htmlFor="note-body">Note</label>
-                    <input id="note-bofy" type="text" placeholder="Let's see, what was so important..."/>
+                    <input id="note-body" type="text" placeholder="Let's see, what was so important..." onChange={e => this.updateBody(e.target.value)} />
                     <input type="submit"/>
                 </form>
 		);
